973d03d2b57160b87236bba367c39cae201dad27
Ronit Yadav <90141095+Ronit-yadav@users.noreply.github.com>
2024-06-27 14:27:26 +0530

    Update render.js
    changed address

--- views/render.js
+++ views/render.js
@@ -2,9 +2,11 @@ const fs = require('fs');
 const ejs = require('ejs');
 const path = require('path');
 
-const inputFilePath = path.join(__dirname, 'path/to/your/file.ejs');
-const outputFilePath = path.join(__dirname, 'path/to/your/output/file.html');
+// Define the path to your EJS file and the output HTML file
+const inputFilePath = path.join(__dirname, 'views', 'dashboard.ejs');
+const outputFilePath = path.join(__dirname, 'dashboard.html');
 
+// Render the EJS file to HTML
 ejs.renderFile(inputFilePath, {}, (err, str) => {
   if (err) {
     console.error(err);
